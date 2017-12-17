c3e633bfe37ae030eedb87c2523a4481ecb752ac
李世举 <425774863@qq.com>
2017-12-18 00:33:07 +0800

    首页布局

--- DouYuZB_LSJ/DouYuZB_LSJ/Classes/Home/Controller/HomeViewController.swift
+++ DouYuZB_LSJ/DouYuZB_LSJ/Classes/Home/Controller/HomeViewController.swift
@@ -8,28 +8,79 @@
 
 import UIKit
 
+private let kTitleViewH : CGFloat = 40
+
 class HomeViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    // MARK:- 懒加载属性
+    fileprivate lazy var pageTitleView : PageTitleView = {[weak self] in
+        let titleFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH, width: kScreenW, height: kTitleViewH)
+        let titles = ["推荐", "游戏", "娱乐", "趣玩"]
+        let titleView = PageTitleView(frame: titleFrame, titles: titles)
+        titleView.delegate = self
+        return titleView
+        }()
+    fileprivate lazy var pageContentView : PageContentView = {[weak self] in
+        
+        // 1.确定内容的frame
+        let contentH = kScreenH - kStatusBarH - kNavigationBarH - kTitleViewH - kTabbarH
+        let contentFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH + kTitleViewH, width: kScreenW, height: contentH)
+        
+        // 2.确定所有的子控制器
+        var childVcs = [UIViewController]()
+        childVcs.append(RecommendViewController())
+        childVcs.append(GameViewController())
+        childVcs.append(AmuseViewController())
+        childVcs.append(FunnyViewController())
+        
+        let contentView = PageContentView(frame: contentFrame, childVcs: childVcs, parentViewController: self)
+        contentView.delegate = self
+        return contentView
+        }()
+}
+//MARK 设置UI
+extension HomeViewController {
+    fileprivate func setUI() {
+        //不需要调整UIScrollView的内边距
+        automaticallyAdjustsScrollViewInsets = false
+        // 1.设置导航栏
+        setupNavigationBar()
+        
+         //2.添加TitleView
+        view.addSubview(pageTitleView)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//        // 3.添加ContentView
+        view.addSubview(pageContentView)
     }
-    */
+    fileprivate func setupNavigationBar() {
+        // 1.设置左侧的Item
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "logo")
+        // 2.设置右侧的Item
+        let size = CGSize(width: 40, height: 40)
+        let historyItem = UIBarButtonItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: size)
+        let searchItem = UIBarButtonItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: size)
+        let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
+        navigationItem.rightBarButtonItems = [historyItem, searchItem, qrcodeItem]
+    }
+}
+// MARK:- 遵守PageTitleViewDelegate协议
+extension HomeViewController : PageTitleViewDelegate {
+    func pageTitleView(_ titleView: PageTitleView, selectedIndex index: Int) {
+        pageContentView.setCurrentIndex(index)
+    }
+}
 
+
+// MARK:- 遵守PageContentViewDelegate协议
+extension HomeViewController : PageContentViewDelegate {
+    func pageContentView(_ contentView: PageContentView, progress: CGFloat, sourceIndex: Int, targetIndex: Int) {
+        pageTitleView.setTitleWithProgress(progress, sourceIndex: sourceIndex, targetIndex: targetIndex)
+    }
 }
---
+++ DouYuZB_LSJ/DouYuZB_LSJ/Classes/Tools/Common/Common.swift
@@ -0,0 +1,16 @@
+//
+//  Common.swift
+//  DouYuZB_LSJ
+//
+//  Created by 李世举 on 12/17/17.
+//  Copyright © 2017 李世举. All rights reserved.
+//
+
+import UIKit
+
+let kStatusBarH : CGFloat = 20
+let kNavigationBarH : CGFloat = 44
+let kTabbarH : CGFloat = 44
+
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
